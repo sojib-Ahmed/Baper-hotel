5d4d8bbe7111da08d02040a672ee105ba3306d50
sojib-Ahmed <sojibahmed.cpd@gmail.com>
2022-02-19 01:37:15 +0600

    diposit item add

--- js/functinoal.js
+++ js/functinoal.js
@@ -1,1 +1,12 @@
-console.log('4444444')
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const valueInText = inputField.value ;
+    const value = parseFloat(valueInText);
+    inputField.value ='';
+    return value;
+}
+
+document.getElementById('deposit-button').addEventListener('click',
+    function () {
+        console.log('depositClick')
+    })
